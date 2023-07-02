eb40c671acad5572d113095d0375b55f78f37b97
Nidal <dwalibi.nidal@gmail.com>
2023-07-02 17:12:27 +0200

    dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='auria',
-    version='0.0.1',
+    version='0.0.2',
     author='N.i.d.a.l',
     description='Auria package',
     packages=find_packages(),
