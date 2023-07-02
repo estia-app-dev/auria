c892d15aae15e1beeb14832e6cff80061db7c085
Nidal <dwalibi.nidal@gmail.com>
2023-07-02 15:14:46 +0200

    dev

---
+++ README.md
@@ -0,0 +1,9 @@
+Installation
+=============
+
+#### Générer un fichier propre requirements.txt
+
+```
+pip install pipreqs
+pipreqs C:\Users\nidal\Documents\Dev\Projects\Auria --force
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='auria',
+    version='0.0.1',
+    author='N.i.d.a.l',
+    description='Auria package',
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
